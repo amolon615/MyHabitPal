7b743b3a9c7c79e03739a1c04dd49331d6daca1c
amolon615 <114776102+amolon615@users.noreply.github.com>
2022-12-06 11:49:46 +0100

    Initial Commit

---
+++ MyHabitPal/MyHabitPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitPalApp.swift
+//  MyHabitPal
+//
+//  Created by Artem on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
